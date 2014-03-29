c9f5a6a2cdc34711c9424a3a572761cb66748a4e
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2014-03-29 19:40:17 +0600

    suggest code in conroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,16 @@ class HomeController < ApplicationController
     end
   end
 
+  def suggest
+    #This is for the API of getting json through the API of IMDB
+    #link: http://lab.abhinayrathore.com/imdb_suggest/
+    #
 
+    key = CGI::escape params[:term].to_s
+    results = open('http://lab.abhinayrathore.com/imdb_suggest/suggest.php?term=' + key)
+    results = JSON.parse(results.string)
+    render json: results
+  end
 
   def proxy_img
     #Proxy route for image to avoid broken images
