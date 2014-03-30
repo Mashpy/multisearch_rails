0bc34043b8fbf3b0f3e830ac7b77c675faf33ca3
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2014-03-30 09:49:46 +0600

    imdb data will show in iframe

--- app/controllers/imdb_controller.rb
+++ app/controllers/imdb_controller.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-
-class WikiController < ApplicationController
+class ImdbController < ApplicationController
+  
   def index
     @title="this is index of def"
 
@@ -11,12 +11,12 @@ class WikiController < ApplicationController
   def show
     @title="this is show of def"
     page = params[:page].to_s
-        wikiurl = "http://en.wikipedia.org" + page
+        wikiurl = "http://www.imdb.com" + page
         doc = Nokogiri::HTML(open(wikiurl))
         @titlearray = []
-        doc.css("#bodyContent").each do |titlecss|
+        doc.css("#overview-top , img").each do |titlecss|
         @titlearray << titlecss.inner_html
      end
   end
-
+  
 end
---
+++ test/controllers/imdb_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ImdbControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
