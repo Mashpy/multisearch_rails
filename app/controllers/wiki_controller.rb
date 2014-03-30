99e5ca9e2fb489c0ae57fb2b654a6afc886fa00b
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2014-03-30 09:08:23 +0600

    wiki page will show in iframe

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
 class WikiController < ApplicationController
   def index
     @title="this is index of def"
@@ -6,7 +10,13 @@ class WikiController < ApplicationController
   
   def show
     @title="this is show of def"
-    @page = params[:page].to_s
+    page = params[:page].to_s
+        wikiurl = "http://en.wikipedia.org" + page
+        doc = Nokogiri::HTML(open(wikiurl))
+        @titlearray = []
+        doc.css("span").each do |titlecss|
+        @titlearray << titlecss.text
+     end
   end
 
 end
