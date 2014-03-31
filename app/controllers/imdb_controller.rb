a86a82ca457c54ee03d89f3928afa66405d851ca
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2014-03-31 07:08:18 +0600

    removed unused css

--- app/controllers/imdb_controller.rb
+++ app/controllers/imdb_controller.rb
@@ -15,6 +15,15 @@ class ImdbController < ApplicationController
         doc = Nokogiri::HTML(open(imdburl))
         @titlearray = []
         doc.css("body").each do |titlecss|
+          doc.css('#navbar , #quicksearch, #navbar-query, #top_ad_wrapper, #bottom_ad_wrapper, .footer, .aux-content-widget-3').remove
+          doc.css(' #prometer').remove
+          doc.css('#name-pro-info , #boardsTeaser, .contribute, .see-more, h2, .subnav, .aux-content-widget-2').remove 
+          doc.css('.rightcornerlink').remove 
+          doc.css('#overview-bottom , .giga-star').remove 
+          doc.css('#ynd_557422').remove 
+          doc.css('#tn15crumbs , #tn15lhs, #action-box').remove 
+          
+               
           titlecss.css('a').each do |anc|
           if anc.attributes['href'].try(:value).present?
             anc.attributes['href'].value = 'http://www.imdb.com' + anc.attributes['href'].value
