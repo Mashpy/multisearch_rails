4be6cad6fb64184b5d1dc32236a1a40488aac1e8
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2014-04-03 07:02:35 +0600

    image problem solved

--- app/controllers/imdb_controller.rb
+++ app/controllers/imdb_controller.rb
@@ -27,6 +27,21 @@ class ImdbController < ApplicationController
         end
         @titlearray << titlecss.inner_html
      end
+     doc = Nokogiri::HTML(open(imdburl))
+        @imagearray = []
+
+        doc.css("#img_primary").each do |imagecss|
+        doc.xpath('//@style').remove 
+        doc.css(".text-center").remove
+  
+        imagecss.css('a').each do |anc|
+          if anc.attributes['href'].try(:value).present?
+            anc.attributes['href'].value = 'http://www.imdb.com' + anc.attributes['href'].value
+           anc['target'] = '_blank'
+          end
+        end
+        @imagearray << imagecss.inner_html
+     end
   end
   
 end
