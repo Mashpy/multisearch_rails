a34e6990a2b9c246bcc480031d854c9f40bbdbbb
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2014-04-03 16:48:21 +0600

    testing 1

--- app/controllers/imdb_controller.rb
+++ app/controllers/imdb_controller.rb
@@ -16,9 +16,8 @@ class ImdbController < ApplicationController
         @titlearray = []
         doc.css("body").each do |titlecss|
           doc.xpath('//@style').remove 
-          doc.css('#img_primary a , .star-box-rating-widget, #ynd_2745629, #prometer, .star-box-giga-star, #overview-bottom, #titleRecs, .contribute, #boardsTeaser, #titleFAQ, #titleDidYouKnow, #footer, #maindetails_sidebar_bottom, #maindetails_sidebar_bottom .odd, #maindetails_sidebar_bottom .even, .aux-content-widget-2, .subnav, #top_ad_wrapper, #navbar').remove
-          doc.css('#moka_form .aux-content-widget-3').remove
-          doc.css('.injected_slot').remove     
+          doc.css('body').remove
+
           titlecss.css('a').each do |anc|
           if anc.attributes['href'].try(:value).present?
             anc.attributes['href'].value = 'http://www.imdb.com' + anc.attributes['href'].value
@@ -27,21 +26,7 @@ class ImdbController < ApplicationController
         end
         @titlearray << titlecss.inner_html
      end
-     doc = Nokogiri::HTML(open(imdburl))
-        @imagearray = []
 
-        doc.css("#img_primary").each do |imagecss|
-        doc.xpath('//@style').remove 
-        doc.css(".text-center").remove
-  
-        imagecss.css('a').each do |anc|
-          if anc.attributes['href'].try(:value).present?
-            anc.attributes['href'].value = 'http://www.imdb.com' + anc.attributes['href'].value
-           anc['target'] = '_blank'
-          end
-        end
-        @imagearray << imagecss.inner_html
-     end
   end
   
 end
